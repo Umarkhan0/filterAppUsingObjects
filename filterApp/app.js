59b9d7c77b848b623a60d6acdc3ac25af8320c79
Umar khan <125694026+Umarkhan0@users.noreply.github.com>
2023-06-07 16:31:19 +0845

    Add files via upload

---
+++ filterApp/app.js
@@ -0,0 +1,378 @@
+let Bikes = {
+    honda: {
+        HondaActiva6G: {
+            name: "Honda Activa 6G",
+            price: "76,466 - 82,467*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/honda/activa-6g/640X309/activa-6g63ce6a450dd74.jpg?tr=w-300"
+        },
+        HondaSP125: {
+            name: "Honda SP 125",
+            price: "85,733 - 89,733*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/honda/sp-125/494X300/sp-125646c75d0667b8.jpg?imwidth=400&impolicy=resize"
+        },
+        HondaDio: {
+            name: "Honda Dio",
+            price: "72,310 - 78,311*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/honda/dio-bs6/650X420/dio-bs66116106361ff8.jpg?imwidth=400&impolicy=resize"
+        },
+        HondaShine: {
+            name: "Honda Shine",
+            price: "79,340 - 83,340*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/honda/shine-bs6/494X300/shine-bs66257c586dbbd2.jpg?imwidth=400&impolicy=resize"
+        },
+        HondaShine100: {
+            name: "HondaShine100",
+            price: "64,900*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/honda/shine-100/494X300/shine-100641177dc760b4.jpg?imwidth=400&impolicy=resize"
+        },
+        HondaUnicorn: {
+            name: "Honda Unicorn",
+            price: "1.07 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/honda/unicorn-bs6/494X300/unicorn-bs6625919b832196.jpg?imwidth=400&impolicy=resize"
+        }
+    },
+    Bajaj: {
+        BajajPulsarNS200: {
+            name: "Bajaj Pulsar NS200",
+            price: "1.38 - 1.49 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/bajaj/bajaj-pulsar-200-ns/640X309/bajaj-pulsar-200-ns64104b8eb1b68.jpg?tr=w-300"
+        },
+        BajajPulsar125: {
+            name: "Bajaj Pulsar 125",
+            price: "80,666 - 87,437*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/bajaj/pulsar-125/494X300/pulsar-1255fd98986e7a39.jpg?imwidth=400&impolicy=resize"
+        },
+        BajajPulsarN160: {
+            name: "Bajaj Pulsar RS200",
+            price: "1.46 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/bajaj/bajaj-pulsar-rs-200/494X300/bajaj-pulsar-rs-2005fd990850af47.jpg?imwidth=400&impolicy=resize"
+        },
+        BajajPulsarRS200: {
+            name: "Bajaj Pulsar N160",
+            price: "1.23 - 1.31 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/bajaj/pulsar-n160/494X300/pulsar-n16062d8fe9f8d8a6.jpg?imwidth=400&impolicy=resize"
+        },
+        BajajPulsar150: {
+            name: "Bajaj Pulsar 150",
+            price: "1.07 - 1.10 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/bajaj/bajaj-pulsar-150/494X300/bajaj-pulsar-15060eed160c65fe.jpg?imwidth=400&impolicy=resize"
+        },
+        BajajPulsarNS125: {
+            name: "Bajaj Pulsar NS 125",
+            price: "1.07 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/bajaj/pulsar-ns-125/494X300/pulsar-ns-125611b855108045.jpg?imwidth=400&impolicy=resize"
+        }
+    },
+    TVSBikes: {
+        TVSRaider: {
+            name: "TVS Raider",
+            price: "86,771 - 1.03 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/tvs/raider/640X309/raider6437f733e3c37.jpg?tr=w-300"
+        },
+        TVSNTORQ125: {
+            name: "TVS NTORQ 125",
+            price: "86,347 - 1.07 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/tvs/ntorq-125/494X300/ntorq-1256148516350f91.jpg?imwidth=400&impolicy=resize"
+        },
+        TVSApacheRTR160: {
+            name: "TVS Apache RTR 160",
+            price: "1.18 - 1.25 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/tvs/apache-160/650X420/apache-1606319c53d7f639.jpg?imwidth=400&impolicy=resize"
+        },
+        TVSJupiter: {
+            name: "TVS Jupiter",
+            price: "74,656 - 88,901*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/tvs/tvs-jupiter/494X300/tvs-jupiter611c8e3c80cbb.jpg?imwidth=400&impolicy=resize"
+        },
+        TVSiQubeElectric: {
+            name: "TVS iQube Electric",
+            price: "1.63 - 1.65 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/tvs/iqube-electric/494X300/iqube-electric63368c7e20470.jpg?imwidth=400&impolicy=resize"
+        },
+        TVSApacheRTR1604V: {
+            name: "TVS Apache RTR 160 4V",
+            price: "1.24 - 1.45 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/tvs/tvs-apache/494X300/tvs-apache6177a607f4015.jpg?imwidth=400&impolicy=resize"
+        }
+    },
+    HeroBikes: {
+        HeroSplendorPlus: {
+            name: "Hero Splendor Plus",
+            price: "73,531 - 74,771*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/hero-motocorp/hero-motocorp-splendor/640X309/hero-motocorp-splendor628c6c03932f2.jpg?tr=w-300"
+        },
+        HeroHFDeluxe: {
+            name: "Hero HF Deluxe",
+            price: "61,708 - 68,858*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/hero/hf-delux-bs6/494X300/hf-delux-bs661821cf60d5be.jpg?imwidth=400&impolicy=resize"
+        },
+        HeroSuperSplendor: {
+            name: "Hero Super Splendor",
+            price: "80,198 - 84,838*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/hero-motocorp/hero-motocorp-super-splendor/494X300/hero-motocorp-super-splendor632191f414a01.jpg?imwidth=400&impolicy=resize"
+        },
+        HeroPassionPro: {
+            name: "Hero Passion Pro",
+            price: "73,428 - 82,903*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/hero/passion-pro-110/494X300/passion-pro-110611641ea57d7e.jpg?imwidth=400&impolicy=resize"
+        },
+        HeroSplendorPlusXtec: {
+            name: "Hero Splendor Plus Xtec",
+            price: "78,251*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/hero/splendor-plus-xtec/494X300/splendor-plus-xtec63fca32ec7eb3.jpg?imwidth=400&impolicy=resize"
+        },
+        HeroXPulse2004V: {
+            name: "Hero XPulse 200 4V",
+            price: "1.39 - 1.51 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/hero/xpulse-200/494X300/xpulse-2006464ccccd02ec.jpg?imwidth=400&impolicy=resize"
+        }
+    },
+    KTMBikes: {
+        KTM390Duke: {
+            name: "KTM 390 Duke",
+            price: "2.94 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/ktm/ktm-duke-390/640X309/ktm-duke-3905fd47cd663191.jpg?tr=w-300"
+        },
+        KTM200Duke: {
+            name: "KTM 200 Duke",
+            price: "1.91 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/ktm/ktm-duke/650X420/ktm-duke5fd45dd351563.jpg?imwidth=400&impolicy=resize"
+        },
+        KTM250Duke: {
+            name: "KTM 250 Duke",
+            price: "2.37 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/ktm/duke-250/494X300/duke-2505fd47b2feb975.jpg?imwidth=400&impolicy=resize"
+        },
+        KTM125Duke: {
+            name: "KTM 125 Duke",
+            price: "1.77 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/ktm/125-duke/650X420/125-duke5fd45a71e5b38.jpg?imwidth=400&impolicy=resize"
+        },
+        KTMRC390: {
+            name: "KTM RC 390",
+            price: "3.16 - 3.18 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/ktm/2021-rc-390/494X300/2021-rc-39062e3955a6678e.jpg?imwidth=400&impolicy=resize"
+        },
+        KTMRC200: {
+            name: "KTM RC 200",
+            price: "2.14 - 2.17 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/ktm/2021-rc-200/494X300/2021-rc-200616eb234421de.jpg?imwidth=400&impolicy=resize",
+
+
+        }
+    },
+    RoyalEnfieldBikes: {
+        RoyalEnfieldHunter350: {
+            name: "Enfield Hunter 350",
+            price: "1.50 - 1.75 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/royal-enfield/roadstar/640X309/roadstar62efbea09ec54.jpg?tr=w-300"
+        },
+        RoyalEnfieldClassic350: {
+            name: "Enfield Classic 350",
+            price: "1.90 - 2.21 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/royal-enfield/classic350/494X300/classic3506131bb8729f70.jpg?imwidth=400&impolicy=resize"
+        },
+        RoyalEnfieldBullet350: {
+            name: "Enfield Bullet 350",
+            price: "Rs.1.48 - 1.63 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/royal-enfield/royal-enfield-bullet/494X300/royal-enfield-bullet6030de3ce18e4.jpg?imwidth=400&impolicy=resize"
+        },
+        RoyalEnfieldContinentalGT650: {
+            name: "Enfield Continental 650",
+            price: "3.19 - 3.45 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/royal-enfield/continental-gt-650/494X300/continental-gt-650641562f366c8a.jpg?imwidth=400&impolicy=resize"
+        },
+        RoyalEnfieldHimalayan: {
+            name: "Enfield Himalayan",
+            price: "2.16 - 2.28 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/royal-enfield/himalayan-bs6/494X300/himalayan-bs660261c15f2cfa.jpg?imwidth=400&impolicy=resize"
+        },
+        RoyalEnfieldMeteor350: {
+            name: "Enfield Meteor 350",
+            price: "2.01 - 2.22 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/royal-enfield/meteor/494X300/meteor6030d40bd776d.jpg?imwidth=400&impolicy=resize"
+        }
+    },
+    YamahaBikes: {
+        YamahaMT15V2: {
+            name: "Yamaha MT 15 V2",
+            price: "1.66 - 1.70 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/yamaha/mt-15-2-0/640X309/mt-15-2-063ea25b00fa58.jpg?tr=w-300"
+        },
+        YamahaR15V4: {
+            name: "Yamaha R15 V4",
+            price: "1.82 - 1.96 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/yamaha/r15-v4/494X300/r15-v4616fa9ac63d6c.jpg?imwidth=400&impolicy=resize"
+        },
+        YamahaR15S: {
+            name: "Yamaha R15S",
+            price: "1.65 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/yamaha/yzf-r15-v3/494X300/yzf-r15-v362988a686cfba.jpg?imwidth=400&impolicy=resize"
+        },
+        YamahaFZS_FIV3: {
+            name: "Yamaha FZS-FI V3",
+            price: "1.22 - 1.29 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/yamaha/fzs-fi-v3-bs6/494X300/fzs-fi-v3-bs661d2d0eed5761.jpg?imwidth=400&impolicy=resize"
+        },
+        YamahaAerox155: {
+            name: "Yamaha FZ-FI V3",
+            price: "1.17 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/yamaha/fz-fi-version-3/494X300/fz-fi-version-35e687e0aadb74.jpg?imwidth=400&impolicy=resize"
+        },
+        YamahaFZ_FIV3: {
+            name: "Yamaha Aerox 155",
+            price: "1.44 Lakh*",
+            model: 2023,
+            images: "https://bd.gaadicdn.com/processedimages/yamaha/aerox-155/494X300/aerox-155619b81e12487e.jpg?imwidth=400&impolicy=resize"
+        }
+    }
+}
+
+let allBikes = function () {
+    let bikeData = '';
+    for (let bike in Bikes) {
+        for (let completBikeName in Bikes[bike]) {
+            let name = Bikes[bike][completBikeName].name;
+            let image = Bikes[bike][completBikeName].images;
+
+            let price = Bikes[bike][completBikeName].price
+            bikeData += `
+            <div class="printCard">
+                        <img src="${image}" alt="${name}">
+                        <h5>${name}</h5>
+                       
+                        <p>Price: <b> ${price}</b></p>
+                    </div>`;
+        }
+    }
+    document.getElementById("card").innerHTML = bikeData;
+}
+allBikes()
+let work = false
+
+let option = "";
+let nestedObjectName = Object.keys(Bikes);
+for (var ObjectNameObjectName in nestedObjectName) {
+    option += `<option class="option">${nestedObjectName[ObjectNameObjectName]}</option>`
+    document.getElementById("dropdown").innerHTML = `
+<select name="dropdown" onchange="getValue()" id="select">
+                ${option}
+            </select>`
+
+
+}
+let selectData = "";
+function getValue() {
+    let dropdown = document.getElementById("select").value;
+    let oneBikeGet = Bikes[dropdown]
+    for (let dropDownGet in oneBikeGet) {
+        let name = oneBikeGet[dropDownGet].name;
+        let image = oneBikeGet[dropDownGet].images;
+        let price = oneBikeGet[dropDownGet].price
+        selectData += `<div class="printCard">
+                        <img src="${image}" alt="${name}">
+                        <h5>${name}</h5>
+                        
+                        <p>Price: <b> ${price}</b></p>
+                    </div>`
+    }
+    document.getElementById("card").innerHTML = selectData;
+    selectData = "";
+}
+function search() {
+    let input = document.getElementById("input").value;
+    let oneBikeGet = Bikes[input]
+    for (let input in oneBikeGet) {
+        let name = oneBikeGet[input].name;
+        let image = oneBikeGet[input].images;
+        let price = oneBikeGet[input].price
+        selectData += `<div class="printCard">
+                    <h5>${name}</h5>
+                    <img src="${image}" alt="${name}">
+                    
+                    <p>Price: <b> ${price}</b></p>
+                </div>`
+    }
+    document.getElementById("card").innerHTML = selectData;
+    selectData = "";
+    switch (input) {
+        case "Honda Activa 6G":
+            input = "HondaActiva6G"
+            let name = Bikes.honda[input].name;
+            let image = Bikes.honda[input].images;
+            let price = Bikes.honda[input].price
+            selectData = `<div class="printCard">
+                <h5>${name}</h5>
+                <img src="${image}" alt="${name}">
+                <p>Price: ${price}</p>
+            </div>`
+            break;
+        case "Honda SP 125":
+            input = "HondaSP125"
+            let Secname = Bikes.honda[input].name;
+            let secimage = Bikes.honda[input].images;
+            let secprice = Bikes.honda[input].price
+            selectData = `<div class="printCard">
+                <h5>${Secname}</h5>
+                <img src="${secimage}" alt="${Secname}">
+                
+                <p>Price: <b> ${secprice}</b></p>
+            </div>`
+            break;
+        case "Honda Dio":
+            input = "HondaDio"
+            let thirname = Bikes.honda[input].name;
+            let thirdimage = Bikes.honda[input].images;
+            let thirdprice = Bikes.honda[input].price
+            selectData = `<div class="printCard">
+                    <h5>${thirname}</h5>
+                    <img src="${thirdimage}" alt="${thirname}">
+                    <p>Price: <b> ${price}</b></p>
+                </div>`
+        default:
+            document.getElementById("pra").innerHTML = `<h1>WORKING CONTINUE.......</h1>`;
+    }
+    let work = document.getElementById("card").innerHTML = selectData;
+    input = input.innerHTML = "";
+};
